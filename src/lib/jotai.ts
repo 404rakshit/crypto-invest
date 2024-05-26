f6e7d4450899c0b459989a46050ba23d256dc991
Rakshit Raj <404rakshit@gmail.com>
2024-05-26 19:22:20 +0530

    Update jotai.ts

--- src/lib/jotai.ts
+++ src/lib/jotai.ts
@@ -1,6 +1,12 @@
 import { atomWithStorage } from "jotai/utils";
 
 export const user = atomWithStorage("user", {
-    data: null,
+    data: {
+        username: null,
+        fname: null,
+        lname: null,
+        phone: null,
+        email: null
+    },
     lgogedIn: false
 })
