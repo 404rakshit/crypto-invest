a37232334e67c23052e1fbf498a4c4981734a7a4
Rakshit Raj <404rakshit@gmail.com>
2024-07-02 19:30:16 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ReactQuery from "@/util/queryProvider";
+import Footer from "@/components/footer";
+import { Toaster } from "sonner";
 
 const inter = Bricolage_Grotesque({ subsets: ["latin"], display: "swap" });
 
@@ -20,19 +23,19 @@ export default async function RootLayout({
   
   return (
     <html lang="en">
-      <body className={`${inter.className} overflow-x-hidden text-violet-800 flex min-h-screen`}>
+      <body className={`${inter.className} overflow-x-hidden text-violet-800 flex flex-col min-h-screen`}>
         {/* <div className="flex flex-col gap-1 m-auto">
           <Image src="/payment-due.gif" alt="Crypto Invest USA Developer's Payment Due" height={700} width={700} className="aspect-video" />
           <Link target="_blank" href={"mailto:devproject193@gmail.com"}>
             <Button variant={"destructive"} className="w-full">Contact Developer Now</Button>
           </Link>
         </div> */}
-        <span className="text-5xl text-black font-bold m-auto">404 Error</span>
-        {/* <ReactQuery>
+        {/* <span className="text-5xl text-black font-bold m-auto">404 Error</span> */}
+        <ReactQuery>
           {children}
           <Footer />
           <Toaster richColors />
-        </ReactQuery> */}
+        </ReactQuery>
       </body>
     </html>
   );
